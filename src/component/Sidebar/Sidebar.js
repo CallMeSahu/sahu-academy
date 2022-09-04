98bbf07775b830e60e9e8a85a05a59d329a143a6
Siddhartha <siddharthasahu.work@gmail.com>
2022-09-04 19:49:51 +0530

    feat: sidebar

--- src/component/Sidebar/Sidebar.js
+++ src/component/Sidebar/Sidebar.js
@@ -1,7 +1,69 @@
+import React from 'react';
+import "./Sidebar.css";
+import { NavLink } from "react-router-dom";
+import { useData } from "../../context/data/videoContext";
 
 export function Sidebar() {
-  
+  const{ drawer, setDrawer } = useData();
+
   return (
-    <div></div>
+    <div className={`sidebar-container ${drawer ? "trans-on" : "trans-off"}`}>
+      <ul>
+        <li onClick={() => setDrawer(!drawer)}>
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? "sidebar-option active" : "sidebar-option"
+            }
+          >
+            <i className="fa fa-home" aria-hidden="true"></i>
+            Home
+          </NavLink>
+        </li>
+
+        <li onClick={() => setDrawer(!drawer)}>
+          <NavLink
+            to="/playlist"
+            className={({ isActive }) =>
+              isActive ? "sidebar-option active" : "sidebar-option"
+            }
+          >
+            <i className="fa fa-play-circle" aria-hidden="true"></i>Playlist
+          </NavLink>
+        </li>
+
+        <li onClick={() => setDrawer(!drawer)}>
+          <NavLink
+            to="/liked"
+            className={({ isActive }) =>
+              isActive ? "sidebar-option active" : "sidebar-option"
+            }
+          >
+            <i className="fa fa-heart" aria-hidden="true"></i>Liked
+          </NavLink>
+        </li>
+
+        <li onClick={() => setDrawer(!drawer)}>
+          <NavLink
+            to="/watchLater"
+            className={({ isActive }) =>
+              isActive ? "sidebar-option active" : "sidebar-option"
+            }
+          >
+            <i className="fa fa-clock-o" aria-hidden="true"></i>Watch Later
+          </NavLink>
+        </li>
+        <li onClick={() => setDrawer(!drawer)}>
+          <NavLink
+            to="/history"
+            className={({ isActive }) =>
+              isActive ? "sidebar-option active" : "sidebar-option"
+            }
+          >
+            <i className="fa fa-history" aria-hidden="true"></i>History
+          </NavLink>
+        </li>
+      </ul>
+    </div>
   );
 }
