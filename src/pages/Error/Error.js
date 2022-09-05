c323473f3652ee704f77eeb1ea27071b96861ce9
Siddhartha <siddharthasahu.work@gmail.com>
2022-09-06 01:10:28 +0530

    feat: error

--- src/pages/Error/Error.js
+++ src/pages/Error/Error.js
@@ -1,7 +1,12 @@
+import React from "react";
+import "./Error.css";
+import error from "../../assets/error.svg"
 
 export function Error() {
   
   return (
-    <div></div>
+    <div className="video-list-container error-container">
+      <img src={error} className="responsive-image" />
+    </div>
   );
 }
